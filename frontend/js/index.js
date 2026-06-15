ad289deb207e5e10565672b1ed0d1c9d372da53a
Nathanael Schaefer <nathanael.rschaefer@gmail.com>
2026-06-14 22:45:54 -0500

    Feat: character bust can now be selected/unselected. Buttons react to the selections

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -14,4 +14,22 @@ async function init(api) {
 
 function wire_buttons(api) {
     on_click('theme-btn', toggle_theme);
+
+    document.querySelectorAll('.char-bust').forEach(bust => {
+        bust.addEventListener('click', () => {
+            const isSelected = bust.classList.contains('selected')
+            document.querySelectorAll('.char-bust').forEach(b => b.classList.remove('selected'));
+            if (!isSelected) bust.classList.add('selected');  
+            sync_buttons();
+        });
+    });
 }
+
+function sync_buttons() {
+    const anySelected = document.querySelector('.char-bust.selected') !== null;
+    document.querySelectorAll('.btn[data-requires-selection]').forEach(btn => {
+        btn.classList.toggle('disabled', !anySelected)
+    })
+}
+
+
