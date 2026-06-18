b02d02dfe321ef572a7f25fd83a4946978da06f6
Nathanael Schaefer <nathanael.rschaefer@gmail.com>
2026-06-17 23:29:39 -0500

    Feat: added new item skeleton, need function

--- frontend/js/inventory.js
+++ frontend/js/inventory.js
@@ -35,9 +35,10 @@ function wire_buttons(api) {
 
     // Add item to inventory button
     on_click('add-item-btn', () => open_sheet());
-
-    // Sheet closing functionality
     on_click('add-item-overlay', () => close_sheet());
+
+    // New item rendering in the HTML
+    on_click('item-creation-btn', () => show_create_form());
 
 }
 
@@ -45,7 +46,6 @@ function wire_buttons(api) {
 function open_item_drawer(item) {
     document.getElementById('item-drawer').classList.add('active');
 }
-
 function close_item_drawer() {
     document.querySelectorAll('.inv-item').forEach(i => i.classList.remove('selected'));
     document.getElementById('item-drawer').classList.remove('active');
@@ -55,8 +55,32 @@ function open_sheet() {
     document.getElementById('add-item-overlay').classList.add('active');
     document.getElementById('add-item-sheet').classList.add('active');
 }
-
 function close_sheet() {
     document.getElementById('add-item-overlay').classList.remove('active');
     document.getElementById('add-item-sheet').classList.remove('active');
+}
+
+function show_create_form() {
+    document.getElementById('sheet-detail-view').style.display = 'none';
+    document.getElementById('sheet-create-form').style.display = 'block';
+
+    document.getElementById('sheet-detail-default').style.display = 'none';
+    document.getElementById('sheet-detail-edit-footer').style.display = 'none';
+    document.getElementById('sheet-detail-add-footer').style.display = 'block';
+}
+function show_edit_form() {
+    document.getElementById('sheet-detail-view').style.display = 'none';
+    document.getElementById('sheet-create-form').style.display = 'block';
+
+    document.getElementById('sheet-detail-default').style.display = 'none';
+    document.getElementById('sheet-detail-edit-footer').style.display = 'block';
+    document.getElementById('sheet-detail-add-footer').style.display = 'none';
+}
+function show_detail_form() {
+    document.getElementById('sheet-detail-view').style.display = 'block';
+    document.getElementById('sheet-create-form').style.display = 'none';
+
+    document.getElementById('sheet-detail-default').style.display = 'block';
+    document.getElementById('sheet-detail-edit-footer').style.display = 'none';
+    document.getElementById('sheet-detail-add-footer').style.display = 'none';
 }
